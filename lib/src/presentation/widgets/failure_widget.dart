6882782e6eaa04b4e8c4375522e28dfa2dd7bbf8
dtussupbayev <daulet.tusupbaev@gmail.com>
2024-09-25 03:01:34 +0500

    ui: improve design of failure widget

--- lib/src/presentation/widgets/failure_widget.dart
+++ lib/src/presentation/widgets/failure_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FailureWidget extends StatelessWidget {
-  final String? errorMessage;
+  final String errorMessage;
   final VoidCallback? onRetry;
 
   const FailureWidget({
@@ -13,30 +13,48 @@ class FailureWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.error,
-            color: Colors.grey,
-          ),
-          SizedBox(height: 16),
-          Text(
-            errorMessage ?? '',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.grey,
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+        decoration: BoxDecoration(
+          color: Colors.red.withOpacity(0.1), // Light red background
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.error,
+              color: Colors.red,
+              size: 48.0,
             ),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(height: 16),
-          onRetry != null
-              ? ElevatedButton(
-                  onPressed: onRetry,
-                  child: Text('Retry'),
-                )
-              : SizedBox(),
-        ],
+            SizedBox(height: 8.0),
+            Text(
+              errorMessage,
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 8.0),
+            onRetry != null
+                ? ElevatedButton.icon(
+                    onPressed: onRetry,
+                    icon: Icon(Icons.refresh),
+                    label: Text('Retry'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  )
+                : SizedBox(),
+          ],
+        ),
       ),
     );
   }
